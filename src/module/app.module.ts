da17031ec49b6852db04f84a9fbb0b1b3094eaa0
zhongwu <xingya.sxy@alibaba-inc.com>
2023-07-16 21:36:59 +0800

    feat:Use supabase dn

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from '../controller/app.controller';
 import { AppService } from '../service/app.service';
+import { PrismaService } from '../service/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
